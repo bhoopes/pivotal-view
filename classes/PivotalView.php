e96be37a92305ee44fdec48ddf07942bea817445
Brian Hoopes <bhoopes@deseretdigital.com>
2011-09-15 15:30:20 -0600

    Added getStories, getStoriesById, getProjects, and getProjectsById

--- classes/PivotalView.php
+++ classes/PivotalView.php
@@ -2,10 +2,27 @@
 class PivotalView
 {
 	private $token;
+	private $baseUrl = "https://www.pivotaltracker.com/services/v3/";
 
 	public function __construct()
 	{
 		$this->token = file_get_contents(realpath(dirname(dirname(__FILE__)))."/pivotal-key.txt");
+	}
+
+	private function makeRequest($path)
+	{
+		$headerArray = array("X-TrackerToken: ".$this->token);
+
+		$curl = curl_init();
+		$URL = $this->baseUrl.$path;
+		curl_setopt($curl, CURLOPT_URL, "$URL");
+		curl_setopt($curl, CURLOPT_HTTPHEADER, array("X-TrackerToken: ".$this->token));
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		//curl_setopt($curl, CURLOPT_HEADER, 0);
+		$string = curl_exec($curl);
+		curl_close($curl);
+
+		return $string;
 	}
 
 	public function getToken()
@@ -13,5 +30,65 @@ class PivotalView
 		return $this->token;
 	}
 
+	private function parseXML($xml)
+	{
+		$xml = new SimpleXMLElement($xml);
+		return $xml;
+	}
+
+
+	public function getProjects()
+	{
+		$projects = $this->makeRequest("projects");
+		$projects = $this->parseXML($projects);
+		foreach($projects->project AS $item)
+		{
+			$items[] = $item;
+		}
+		return $items;
+
+		echo "<pre>";
+		print_r($items);
+		echo "</pre>";
+		echo "<br /><br />";
+		foreach($items AS $item)
+		{
+			echo $item->name."<br />";
+		}
+	}
+
+	public function getProjectById($id)
+	{
+		$project = $this->makeRequest("projects/$id");
+		$project = $this->parseXML($project);
+		foreach($project->project AS $item)
+		{
+			$items[] = $item;
+		}
+		return $items;
+
+	}
+
+	public function getStories($projectId)
+	{
+		$stories = $this->makeRequest("projects/$projectId/stories");
+		$stories = $this->parseXML($stories);
+		foreach($stories->story AS $item)
+		{
+			$items[] = $item;
+		}
+		return $items;
+	}
+
+	public function getStoryById($projectId, $storyId)
+	{
+		$projects = $this->makeRequest("projects/$projectId/stories");
+		$projects = $this->parseXML($projects);
+		foreach($projects->project AS $item)
+		{
+			$items[] = $item;
+		}
+		return $items;
+	}
 }
 ?>
--- public/index.php
+++ public/index.php
@@ -3,13 +3,32 @@
 </head>
 <body>
 <h1>Pivotal View</h1>
-
 <?
+//phpinfo();
 	require_once('../classes/PivotalView.php');
 
 	$pv = new PivotalView();
 
-	echo "Token: x".$pv->getToken()."x<br />";
+	//echo "Token: ".$pv->getToken()."<br /><br />";
+	
+?>
+<h3>Projects</h3>
+<?
+	$projects = $pv->getProjects();
+	foreach($projects AS $project)
+	{
+		echo $project->name."<br />";
+		$stories = $pv->getStories($project->id);
+		if(count($stories))
+		{
+			echo "<ul>";
+			foreach($stories AS $story)
+			{
+				echo "<li>".$story->name."(".$story->current_state.")</li>";
+			}
+			echo "</ul>";
+		}
+	}
 ?>
 </body>
 </html>
